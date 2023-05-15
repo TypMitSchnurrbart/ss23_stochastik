680a2724e28ac40607af5cc3cb4d82113fbbdba2
TypMitSchnurrbart <mueller.alexan-it19@it.dhbw-ravensburg.de>
2023-05-15 13:53:22 +0000

    Starting new project

---
+++ hero_generator/main.py
@@ -0,0 +1,27 @@
+"""
+    Module to generate stats for heros,
+    that are correlating!
+
+    author:     Marco Mollo,
+                Nick Nowak,
+                Manuel Guenter
+                Alexander Mueller
+
+    date:       15.05.2023
+    version:    0.0.1
+
+    TODO
+        - Understand the task
+        - Find a conecept of hero versions
+        - Implement a generator based on the chosen concept
+
+"""
+
+#===== IMPORTS =======================================
+
+#===== FUNCTIONS =====================================
+
+#===== MAIN ==========================================
+if __name__ == "__main__":
+
+    exit()
